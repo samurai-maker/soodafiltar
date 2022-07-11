2bc5c7c6768b36d3acdc92cdf5766fb6df5e7018
samurai-maker <84018991+samurai-maker@users.noreply.github.com>
2022-07-11 03:15:24 -0700

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -1,58 +1,102 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# (c) @AlbertEinsteinTG
+# (c) @world_wide_movies
 
 class Translation(object):
     
-    START_TEXT = """<b>Hey {}!!</b>
-<i>Am Just A Advance Auto Filter Bot....😉
+    START_TEXT = """
+<b>
+╔══╦══╦══╦══╦══╦══╦══╗
+╚══╩══╩══╩══╩══╩══╩══╝
 
-Just Add Me To Your Group And Channel And Connect Them And See My Pevers 🔥🔥😝
+   █░█░█ █░█░█ █▀▄▀█
+   ▀▄▀▄▀ ▀▄▀▄▀ █░▀░█
 
-For More Details Click Help Button Below..
-@CrazyBotsz
-</i>"""    
+╔══╦══╦══╦══╦══╦══╦══╗
+╚══╩══╩══╩══╩══╩══╩══╝
+</b>
+<b>Hey {}!!</b>
+
+➥<code><i>Am Just A filter manager 
+  Bot of </i></code>
+  <b>@world_wide_movies </b>
+
+➥<code><i>Send the exact Movie name in 
+  our group,I'll reply the 
+  file in PM if available 
+  in our channel
+  
+  thank you ❤️..</code>
+
+▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ 
+<code><i>For More Details Click Help 
+Button Below..  </i> </code>
+▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ 
+
+
+"""    
     
     HELP_TEXT = """
+<b>
+╔══╦══╦══╦══╦══╦══╦══╗
+╚══╩══╩══╩══╩══╩══╩══╝
+
+   █░█░█ █░█░█ █▀▄▀█
+   ▀▄▀▄▀ ▀▄▀▄▀ █░▀░█
+
+╔══╦══╦══╦══╦══╦══╦══╗
+╚══╩══╩══╩══╩══╩══╩══╝
+
+</b>
+
 <b><i><u>How To Use Me!?</u></i></b>
 
 <i>
--> Add Me To Any Group And Make Me Admin
--> Add Me To Your Desired Channel
-</i>
+➥ Add Me To Any Group And Make Me Admin
+➥ Add Me To Your Desired Channel
+
 
 <b>Bot Commands (Works Only In Groups) :</b>
 
-    -> <code>/add chat_id</code>
+    ➥ <code>/add chat_id</code>
                 OR                  - To Connect A Group With A Channel (Bot Should Be Admin With Full Previlages In Both Group And Channel)
      <code>/add @Username</code>
      
-    -> <code>/del chat_id</code>
+    ➥ <code>/del chat_id</code>
                 OR                  - To disconnect A Group With A Channel
      <code>/del @Username</code>
      
-    -> <code>/delall</code>  - This Command Will Disconnect All Connected Channel With The Group And Deletes All Its File From DB
+    ➥ <code>/delall</code>  - This Command Will Disconnect All Connected Channel With The Group And Deletes All Its File From DB
     
-    -> <code>/settings</code> -  This Command Will Display You A Settings Pannel Instance Which Can Be Used To Tweek Bot's Settings Accordingly
+    ➥ <code>/settings</code> -  This Command Will Display You A Settings Pannel Instance Which Can Be Used To Tweek Bot's Settings Accordingly
 
-            -> <code>Channel</code> - Button Will Show You All The Connected Chats With The Group And Will Show Buttons Correspnding To There Order For Furthur Controls
+            ➥ <code>Channel</code> - Button Will Show You All The Connected Chats With The Group And Will Show Buttons Correspnding To There Order For Furthur Controls
             
-            -> <code>Filter Types</code> - Button Will Show You The 3 Filter Option Available In Bot... Pressing Each Buttons Will Either Enable or Disable Them And This Will Take Into Action As Soon As You Use Them Without The Need Of A Restart
+            ➥ <code>Filter Types</code> - Button Will Show You The 3 Filter Option Available In Bot... Pressing Each Buttons Will Either Enable or Disable Them And This Will Take Into Action As Soon As You Use Them Without The Need Of A Restart
 
-            -> <code>Configure</code> - Button Will Helps You To Change No. of Pages/ Buttons Per Page/ Total Result Without Acutally Editing The Repo... Also It Provide Option To Enable/Disable For Showing Invite Link In Each Results
+            ➥ <code>Configure</code> - Button Will Helps You To Change No. of Pages/ Buttons Per Page/ Total Result Without Acutally Editing The Repo... Also It Provide Option To Enable/Disable For Showing Invite Link In Each Results
             
-            -> <code>Status</code> - Button Will Shows The Stats Of Your Channel
-            
-@CrazyBotsz
+            ➥ <code>Status</code> - Button Will Shows The Stats Of Your Channel
+</i>           
+<b>@world_wide_movies <b>
 """
     
-    ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
-    
-<b>➥ Creator</b> : <b><i><a href="https://t.me/AlbertEinstein_TG">AlbertEinstein_TG</a></i></b>
+    ABOUT_TEXT = """
+<b>
+╔══╦══╦══╦══╦══╦══╦══╗
+╚══╩══╩══╩══╩══╩══╩══╝
 
+   █░█░█ █░█░█ █▀▄▀█
+   ▀▄▀▄▀ ▀▄▀▄▀ █░▀░█
+
+╔══╦══╦══╦══╦══╦══╦══╗
+╚══╩══╩══╩══╩══╩══╩══╝
+
+</b>
+
+<b>➥ Name</b> : <code> Auto Filter Bot</code>
+<b>➥ Creator</b> : <b><i><a href="https://github.com/DevAXD">DevAXD</a></i></b>
 <b>➥ Language</b> : <code>Python3</code>
-
 <b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
-
-<b>➥ Source Code</b> : <i><a href="https://github.com/AlbertEinsteinTG/Adv-Auto-Filter-Bot">Click Me</a></i>
+<b>➥ Source Code</b> : <i><a href="https://github.com/DevAXD/fiLTER">Click Me</a></i>
 """
